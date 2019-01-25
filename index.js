2bcaa2b2b08f1feeff277f3150744347596aeadd
Eric Liu <eric.liu@pwc.com>
2019-01-25 17:08:39 +0800

    changes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express   =   require( 'express' );
 var multer    =   require( 'multer' );
-var upload    =   multer( { dest: 'uploads/' } );
 var sizeOf    =   require( 'image-size' );
 var exphbs    =   require( 'express-handlebars' );
 require( 'string.prototype.startswith' );
@@ -8,6 +7,7 @@ require( 'string.prototype.startswith' );
 var app = express();
 
 app.use( express.static( __dirname + '/bower_components' ) );
+app.use( express.static(__dirname + '/public') );
 
 app.engine( '.hbs', exphbs( { extname: '.hbs' } ) );
 app.set('view engine', '.hbs');
@@ -16,21 +16,20 @@ app.get( '/', function( req, res, next ){
   return res.render( 'index' );
 });
 
+var storage = multer.diskStorage(
+    {
+        destination: './uploads/',
+        fileSize: 100,
+        filename: function ( req, file, cb ) {
+            cb( null, file.originalname);
+        }
+    }
+);
+
+
+var upload = multer( { storage: storage } );
+
 app.post( '/upload', upload.single( 'file' ), function( req, res, next ) {
-
-  if ( !req.file.mimetype.startsWith( 'image/' ) ) {
-    return res.status( 422 ).json( {
-      error : 'The uploaded file must be an image'
-    } );
-  }
-
-  var dimensions = sizeOf( req.file.path );
-
-  if ( ( dimensions.width < 640 ) || ( dimensions.height < 480 ) ) {
-    return res.status( 422 ).json( {
-      error : 'The image must be at least 640 x 480px'
-    } );
-  }
 
   return res.status( 200 ).send( req.file );
 });
